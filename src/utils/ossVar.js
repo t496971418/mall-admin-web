dfa4c8959532411f01da88114bd95d514a69b6d4
Terry-zh <496971418@qq.com>
2019-08-19 18:02:28 +0800

    [initial]

--- src/utils/ossVar.js
+++ src/utils/ossVar.js
@@ -1,36 +1,14 @@
-import { currBranch } from '@/projects/menu/api/api.config.js'
+/*
+ * @Author: Terryzh
+ * @Date: 2019-08-19 17:44:45
+ * @LastEditors: Terryzh
+ * @LastEditTime: 2019-08-19 18:02:02
+ * @Description: 添加描述
+ */
 /**
  * @param {Object} ossSts oss存储bucket和region
  */
 export const ossSts = {
-  bucket: 'pd-api-' + currBranch(),
+  bucket: 'ky-api-dev',
   region: 'oss-cn-shenzhen'
 }
-
-export const ossStsExcuApk = {
-  bucket: 'pd-auto-builds-apk',
-  region: 'oss-cn-shenzhen'
-}
-
-export const ossStsFile = {
-  bucket: 'pd-api-' + currBranch(),
-  region: 'oss-cn-shenzhen'
-}
-
-export const tempAuthKey = {
-  accessKeyId: 'LTAI0pGOzOcQSdpF',
-  accessKeySecret: 'BzCXNiLIilYDbPT6WbyNBgUYbIdmxn'
-}
-/**
- * @param {String} defaultPath 默认存储文件夹路径
- */
-export const defaultPath = `org/license`
-
-/**
- * @param {String} PicPath 默认存储文件夹路径
- */
-export const PicPath = `auth/headpic/`
-/**
- * @param {String} uploadUrl 上传的url
- */
-export const uploadUrl = `https://${ossSts.bucket}.${ossSts.region}.aliyuncs.com/`
